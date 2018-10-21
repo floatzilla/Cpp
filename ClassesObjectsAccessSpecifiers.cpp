60d318211e28f7aa1453d335e971d43014616930
floatzilla <john.glueck85@gmail.com>
2018-10-21 09:38:45 -0500

    Created ClassesObjectsAccessSpecifiers

---
+++ ClassesObjectsAccessSpecifiers.cpp
@@ -0,0 +1,25 @@
+//This is the example for Classes Objects and Access Specifiers.
+//John G
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+class Monster //----------------------------Monster Class---------
+{
+public:
+	void growl()
+	{
+		std::cout << "Growl!!!\n";
+	}
+	std::string name;
+};
+
+int main()//-----------------------------------MAIN---------------
+{
+	Monster monster1;
+	monster1.growl();
+	system("PAUSE");
+	return 0;
+
+}
